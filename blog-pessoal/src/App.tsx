1d9f9381e029fd271e3135cd3f2a82edd8be8bf1
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-08-31 10:26:23 -0300

    @RafaelCandeias00 [Task: criando o primeiro componente] {31/08/2022 10h26}

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './paginas/home/Home';
+import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './paginas/home/Home';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
