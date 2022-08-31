a8d77827c576ecb9127fb328305625899427ba89
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-08-31 15:18:04 -0300

    @RafaelCandeias00 [Task: trabalhando com grid e flex-box] {31/08/2022 15:17}

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import './paginas/home/Home';
 import './App.css';
 import { Route } from 'react-router-dom';
+import { Grid } from '@material-ui/core'
 import Home from './paginas/home/Home';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { Paper, Box, Button } from '@material-ui/core';
 import './Home.css';
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-            <h1 className="titulo">Home</h1>
-            <img className="img" src="https://i.imgur.com/H88yIo2.png" alt="" />
+            <Paper>
+                <Box p={2}>
+                    <Box display={"flex"} justifyContent="center">
+                        <h1>Título</h1>
+                    </Box>
+                    <img src="https://pixabay.com/images/id-7413333/" alt="" style={{ width: "100%"}} height="100%"/>
+                    <Box p={2} display={"flex"} justifyContent="center">
+                        <Button variant="contained" color="primary">
+                            Texto 1
+                        </Button>
+                        <Button variant="contained" color="secondary">
+                            Texto 2
+                        </Button>
+                    </Box>
+                </Box>
+            </Paper>
         </>
     );
 }
