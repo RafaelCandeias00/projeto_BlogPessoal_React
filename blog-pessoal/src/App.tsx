a8dbacc43827d9ae1f8679a640d1a71695a7de6e
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-09-02 12:08:36 -0300

    @RafaelCandeias00 [Task: add componente login] {02/09/2022 12h08}

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import Footer from './compenents/estaticos/footer/Footer';
 import Navbar from './compenents/estaticos/navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/*<Route path="/cadastro" element={<CadastroUsuario />} />*/}
+        </Routes>
+      </div>
       <Footer />
-    </>
-  );
+    </Router>
+  )
 }
 
 export default App;
